ec081f98898023ce7f9a5feda0fbeb3771957a66
Rajnandini Tiwari <rajnandini.tiwari@ukg.com>
2024-08-14 15:23:54 +0530

    adding footer and icons

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,9 +5,12 @@ import { LeaveFormComponent } from './components/leave-form/leave-form.component
 import { SalaryComponent } from './components/salary/salary.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
+import { InfoComponent } from './components/info/info.component';
+import { WhyukgComponent } from './components/whyukg/whyukg.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
+   
     {
         path: 'employee/dashboard', component: EmployeeDashboardComponent, children: [{
             path: 'leaves', component: LeaveFormComponent
@@ -28,7 +31,12 @@ export const routes: Routes = [
             path: 'employee-list', component: ProfileComponent
         }, {
             path: 'leave-request', component: ProfileComponent
-        }
+        },
+        {
+            path: 'info', component: InfoComponent
+        },
+        { path: 'whyukg', component: WhyukgComponent },
+    
     ]  
     },
 ];
---
+++ frontend/src/app/components/whyukg/whyukg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whyukg',
+  standalone: true,
+  imports: [],
+  templateUrl: './whyukg.component.html',
+  styleUrl: './whyukg.component.css'
+})
+export class WhyukgComponent {
+
+}
