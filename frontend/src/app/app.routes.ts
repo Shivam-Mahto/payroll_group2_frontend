bfe47790bb3e463a318a1e91ae17b6494a214729
Aghnash Ranjith <aghnash.ranjith@ukg.com>
2024-08-14 16:40:17 +0530

    failed merge 2

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,13 +5,9 @@ import { LeaveFormComponent } from './components/leave-form/leave-form.component
 import { SalaryComponent } from './components/salary/salary.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
-<<<<<<< HEAD
 import { LeaveRequestsComponent } from './components/admin-dashboard-components/leave-requests/leave-requests.component';
-=======
-import { LeaveComponent } from './components/leave/leave.component';
 import { EmployeeListComponent } from './components/admin-dashboard-components/employee-list/employee-list.component';
-import { NewEmployeeComponent } from './components/admin-dashboard-components/new-employee/new-employee.component';
->>>>>>> db5cc3148b14d042bb708f8ca789a9da96a12c0b
+import { LeaveComponent } from './components/leave/leave.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -36,14 +32,8 @@ export const routes: Routes = [
         }, {
             path: 'employee-list', component: EmployeeListComponent
         }, {
-<<<<<<< HEAD
             path: 'leave-request', component: LeaveRequestsComponent
         }
     ]  
-=======
-            path: 'employee-list/add-employee', component: NewEmployeeComponent
-        },
-        ]
->>>>>>> db5cc3148b14d042bb708f8ca789a9da96a12c0b
     },
 ];
