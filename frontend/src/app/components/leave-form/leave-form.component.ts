d004657339d54b4eab9e8ccfed49b92a5ac8de69
Aghnash Ranjith <aghnash.ranjith@ukg.com>
2024-08-14 11:04:25 +0530

    Leave-table ngFor populated

--- frontend/src/app/components/leave-form/leave-form.component.ts
+++ frontend/src/app/components/leave-form/leave-form.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-leave-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './leave-form.component.html',
   styleUrl: './leave-form.component.css'
 })
 export class LeaveFormComponent {
 
+  leavesList = [
+    {
+      name: "Shivam",
+      startDate: "01/02/2024",
+      endDate: "02/02/2024",
+      employeeId: 1,
+      type: 'sick',
+      status: "pending"
+    },
+    {
+      name: "Shivam",
+      startDate: "01/02/2024",
+      endDate: "02/02/2024",
+      employeeId: 2,
+      type: 'sick',
+      status: "pending"
+    },
+    {
+      name: "Shivam",
+      startDate: "01/02/2024",
+      endDate: "02/02/2024",
+      employeeId: 3,
+      type: 'sick',
+      status: "pending"
+    }
+  ]
 }
