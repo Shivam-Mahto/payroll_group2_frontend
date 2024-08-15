d1ab4bdee0d70fe334056bd4e05721149d5fed0e
Shivam Mahto <shivam.mahto@ukg.com>
2024-08-15 22:51:41 +0530

    completed admin dashboard

--- frontend/src/app/components/admin-dashboard-components/employee-list/employee-list.component.ts
+++ frontend/src/app/components/admin-dashboard-components/employee-list/employee-list.component.ts
@@ -1,13 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
 
+  employeeList = [
+    {
+      empId: 1,
+      name: "Jeremy Neigh",
+      role: "Employee",
+      email: "jeremy@gmail.com"
+    },
+    {
+      empId: 2,
+      name: "Shivam",
+      role: "Employee",
+      email: "shivam@gmail.com"
+    },
+    {
+      empId: 3,
+      name: "jack",
+      role: "Accountant",
+      email: "jack@gmail.com"
+    },
+    {
+      empId: 4,
+      name: "Jeremy Neigh",
+      role: "Employee",
+      email: "jeremy@gmail.com"
+    },
+  ]
+
+  searchTerm: string = "";
+  filteredEmployeeList = this.employeeList;
+
+  searchLeaveRequests() {
+    
+    // console.log("called searchLeaveRequest: ", text);
+
+    if (this.searchTerm == "")
+      this.filteredEmployeeList = this.employeeList;
+    else {
+      this.filteredEmployeeList = this.employeeList.filter(employee =>
+        employee.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+  }
 }
--- frontend/src/app/components/salary/salary.component.ts
+++ frontend/src/app/components/salary/salary.component.ts
@@ -1,12 +1,74 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-salary',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './salary.component.html',
   styleUrl: './salary.component.css'
 })
 export class SalaryComponent {
 
+  salaryComponent = 
+    {
+      basic : "6L",
+      hra : "6L",
+      allowances : "40k"
+    }
+
+  salaryList = [
+    {
+      employeeId: 1,
+      name: "Shivam",
+      month: "January",
+      salary: "40,000"
+    },
+    {
+      employeeId: 1,
+      name: "Shivam",
+      month: "February",
+      salary: "40,000"
+    },
+    {
+      employeeId: 1,
+      name: "Shivam",
+      month: "March",
+      salary: "40,000"
+    },
+    {
+      employeeId: 1,
+      name: "Shivam",
+      month: "April",
+      salary: "40,000"
+    }
+  ]
+
+
+  selectedMonth: string = 'All'; // Default to 'All'
+  filteredSalaryList: any[] = this.salaryList;
+
+  months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  onMonthChange(month: string) {
+    console.log(month);
+    this.selectedMonth = month;
+    this.filterSalary();
+  }
+
+  // default javascript date has mmddyyyy format
+  filterSalary() {
+    if (this.selectedMonth === 'All') {
+      this.filteredSalaryList = this.salaryList;
+    } else { 
+      this.filteredSalaryList = this.salaryList.filter(salary => {
+        // console.log(salary.month);
+        return salary.month == this.selectedMonth
+      });
+    }
+  }
 }
